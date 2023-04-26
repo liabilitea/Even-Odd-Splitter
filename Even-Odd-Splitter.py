cb5eeaf1fb6edaf394011276d08640c71d3d5e78
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 15:18:41 +0800

    Strip and convert into int

--- Even-Odd-Splitter.py
+++ Even-Odd-Splitter.py
@@ -1,4 +1,11 @@
 #Read the contents of the txt file and make it into a list
+with open("numbers.txt", "r") as basis_file:
+
+#Remove the line break character from every line and convert string to integer
+    numbers = []
+for line in basis_file:
+    number = int(line.strip())
+    numbers.append(number)
 
 #Write even and odd numbers to different files 
 
